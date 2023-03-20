0e2042644f12511721b6fa3c3e5a142ed1b01519
kewin.taoanh <kewin.taoanh@gmail.com>
2023-03-20 09:21:13 +0100

    initialize spring boot project

---
+++ backend/src/test/java/com/todoApp/backend/demo/BackendTodoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todoApp.backend.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendTodoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
